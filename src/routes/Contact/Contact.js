b5f71c401aaea787f718684a1af350d9f6c545f0
Patrick Jones <forksofpower@gmail.com>
2018-07-25 17:39:09 -0500

    add work experience

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Contact = () => (
     <div className="content-wrapper">
-        <div className="route-body">
-            <h2>Contact</h2>
+        <div className="route-body" style={{textAlign: 'center'}}>
+          <h3>email: pj [at] patrickjones.tech</h3>
         </div>
     </div>
 );
--- src/routes/Work/Work.js
+++ src/routes/Work/Work.js
@@ -1,59 +1,50 @@
 import React from 'react';
 
-let jobs = [
-  {
-    "sortOrder": 0,
-    "startDate": "2016",
-    "endDate": "2016",
-    "title": "The Guide Life",
-    "tags": [ "web development", "ecommerce", "react" ],
-    "description": "The kahless is more dosi now than star. harmless and surprisingly terrifying. Why does the crewmate go?",
-    "image": [ "https://picsum.photos/g/300/200" ],
-    "links": [ "https://patrickjones.tech" ],
-  },
-  {
-    "sortOrder": 1,
-    "startDate": "2016",
-    "endDate": "2018",
-    "title": "Ace Media Productions",
-    "tags": [ "software development", "python", "web scraping", "long-term" ],
-    "description": "Decorate one quarter cup of nachos in a dozen tablespoons of red wine.To the yellow chickpeas add margerine, watermelon, olive oil and whole chicken.",
-    "image": [ "https://picsum.photos/g/300/200" ],
-    "links": [ "https://patrickjones.tech" ],
-  },
-  {
-    "sortOrder": 0,
-    "startDate": "2016",
-    "endDate": "2016",
-    "title": "First Job",
-    "tags": [ "job", "temporary" ],
-    "description": "The kahless is more dosi now than star. harmless and surprisingly terrifying. Why does the crewmate go?",
-    "image": [ "https://picsum.photos/g/300/200" ],
-    "links": [ "https://patrickjones.tech" ],
-  },
-  {
-    "sortOrder": 1,
-    "startDate": "2016",
-    "endDate": "2018",
-    "title": "Latest Job",
-    "tags": [ "job", "long-term" ],
-    "description": "Decorate one quarter cup of nachos in a dozen tablespoons of red wine.To the yellow chickpeas add margerine, watermelon, olive oil and whole chicken.",
-    "image": [ "https://picsum.photos/g/300/200" ],
-    "links": [ "https://patrickjones.tech" ],
-  }
-];
 const Work = () => (
   <div className="content-wrapper">
     <div className="route-body">
-      <h2>Work</h2>
-      {jobs.map( (job, index) => (
-        <div className={`job-${ index % 2 === 0 ? 'right' : 'left' }`}>
-          <img srcSet={job.image[0]} alt=""/>
-          <h2 className="job-title">{job.title}</h2>
-          <p className="job-description">{job.description}</p>
-        </div>
-      ))}
 
+      <h3><strong>Lead Software Developer, Co-Founder</strong></h3>
+      <h4>Opentruk, Inc.</h4>
+      <p>Spring 2015 - Present</p>
+      <ul>
+        <li>Build solutions for food truck owners and aficionados alike.</li>
+      </ul>
+      <br/>
+      <hr/>
+
+      <h3><strong>Data Management Contractor</strong></h3>
+      <p>Summer 2015 - Present</p>
+      <ul>
+        <li>Scrape and clean lead data for clients with customers in the Medical, Veterinary, and Automotive industries.</li>
+        <li><strong>Technologies used:</strong> Python, Celery, Kafka, Docker Swarm, OpenVPN</li>
+      </ul>
+      <br/>
+      <hr/>
+
+      <h3><strong>Full Stack Developer</strong></h3>
+      <h4>Mr.Delivery, USA.</h4>
+      <p>Fall 2013 - Fall 2015</p>
+      <ul>
+        <li>Managed project specification, development and testing for all company software.</li>
+        <li>Communicated directly with outsourced team members to greatly reduce PM hours charged.</li>
+        <li>Led effort to improve UI/UX in client ordering processes resulting in ~20% more successful orders per day, faster ordering and significatly increased customer conversion from ads.</li>
+        <li>Developed full stack solutions for call center agents resulting in higher accuracy and throughput in order processing.</li>
+        <li>Managed a fleet of iPads being used and abused by drivers 24/7.</li>
+      </ul>
+      <br/>
+      <hr/>
+
+      <h3><strong>Database Technologies Intern</strong></h3>
+      <h4>Hoovers Inc.</h4>
+      <p>Fall 2010 - Fall 2012</p>
+      <ul>
+        <li>Participated int Hoovers' 'Imagination Lab' internship program, focusing on representing complex relationships
+          across many different data sources.</li>
+        <li>Used Python, SQL and AllegroGraph to model Hoovers' enormous collection of international business data in a graph database.</li>
+        <li>Created an Ontology and Domain Model for a large preexisting system to model real world business relationships.</li>
+      </ul>
+      <br/>
     </div>
   </div>
 );
